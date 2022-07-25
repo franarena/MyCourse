dbde14f082b5d3e42dd2b3f8eff0669b06c1a369
Francisco Arena <franarena@gmail.com>
2022-07-25 13:37:25 +0200

    Aggiunto ASP Net Core MVC

--- Startup.cs
+++ Startup.cs
@@ -15,6 +15,7 @@ namespace MyCourse
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,10 +25,13 @@ namespace MyCourse
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseStaticFiles();
 
-            app.Run(async (context) =>
+            //app.UseMvcWithDefaultRoute();
+            app.UseMvc(routeBuilder => 
             {
-                await context.Response.WriteAsync("Hello World!");
+                // /courses/detail/5
+                routeBuilder.MapRoute("default","{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
